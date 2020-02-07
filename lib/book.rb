fd2cb7bf1c18af0255b189e71ead36aeb81c130c
Rick Belson <belsonrj@aol.com>
2020-02-07 05:05:17 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,9 @@
 class Book
   attr_accessor :author, :page_count, :genre
+  attr_reader :title
  
   def initialize(title)
     @title = title
-  end
- 
-  def title
-    @title
   end
  
   def turn_page
@@ -14,3 +11,4 @@ class Book
   end
  
 end
+
